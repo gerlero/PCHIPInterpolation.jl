de6db4bd736a1595accf81208890bb9362fd7f0a
jer-j <122048724+jer-j@users.noreply.github.com>
2024-06-28 11:02:25 -0400

    add extrapolation functionality

--- ext/PCHIPInterpolationRecipesBaseExt.jl
+++ ext/PCHIPInterpolationRecipesBaseExt.jl
@@ -3,11 +3,16 @@ module PCHIPInterpolationRecipesBaseExt
 using PCHIPInterpolation
 using RecipesBase
 
-@recipe function _(itp::Interpolator; markershape = :none)
+@recipe function _(itp::Interpolator; markershape=:none)
     @series begin
         markershape := :none
         plotdensity = clamp(10 * length(itp.xs), 1000, 100000)
-        x = range(first(itp.xs), last(itp.xs), length = plotdensity)
+        if itp.extrapolate
+            Δxs = maximum(diff(itp.xs))
+            x = range(first(itp.xs) - 2Δxs, last(itp.xs) + 2Δxs, length=plotdensity)
+        else
+            x = range(first(itp.xs), last(itp.xs), length=plotdensity)
+        end
         return x, itp.(x)
     end
     if markershape !== :none
