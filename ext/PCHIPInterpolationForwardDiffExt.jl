f934241b63e097d8411973ed24d303c0de5d462f
Gabriel Gerlero <gerlero@users.noreply.github.com>
2026-08-17 13:38:12 -0300

    Format with JuliaFormatter

--- ext/PCHIPInterpolationForwardDiffExt.jl
+++ ext/PCHIPInterpolationForwardDiffExt.jl
@@ -4,7 +4,8 @@ using PCHIPInterpolation: Interpolator
 import PCHIPInterpolation: _findinterval
 using ForwardDiff: Dual
 
-@inline _findinterval(itp::Interpolator{<:AbstractRange}, x::Dual) = _findinterval(itp, x.value)
+@inline _findinterval(itp::Interpolator{<:AbstractRange}, x::Dual) =
+    _findinterval(itp, x.value)
 @inline _findinterval(itp::Interpolator, x::Dual) = _findinterval(itp, x.value)
 
 end
